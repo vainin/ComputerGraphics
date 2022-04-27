641b7f048c933141c1fb44ef998ce71616a7cd36
Benjamin <vainin@users.noreply.github.com>
2022-04-27 10:43:08 -0500

    Start of project

---
+++ Arena.js
@@ -0,0 +1,189 @@
+"use strict";
+
+let gl;
+
+let numPositions  = 36;
+
+let texSize = 64;
+
+let flag = true;
+
+let positionsArray = [];
+let colorsArray = [];
+let texCoordsArray = [];
+
+let texture;
+
+let texCoord = [
+    vec2(0, 0),
+    vec2(0, 1),
+    vec2(1, 1),
+    vec2(1, 0)
+];
+
+let vertices = [
+    vec4(-0.5, -0.5,  0.5, 1.0),
+    vec4(-0.5,  0.5, 0.5, 1.0),
+    vec4(0.5,  0.5, 0.5, 1.0),
+    vec4(0.5, -0.5, 0.5, 1.0),
+    vec4(-0.5, -0.5, -0.5, 1.0),
+    vec4(-0.5,  0.5, -0.5, 1.0),
+    vec4(0.5,  0.5, -0.5, 1.0),
+    vec4(0.5, -0.5, -0.5, 1.0)
+];
+
+let vertexColors = [
+    vec4(0.0, 0.0, 0.0, 1.0),  // black
+    vec4(1.0, 0.0, 0.0, 1.0),  // red
+    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
+    vec4(0.0, 1.0, 0.0, 1.0),  // green
+    vec4(0.0, 0.0, 1.0, 1.0),  // blue
+    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
+    vec4(0.0, 1.0, 1.0, 1.0),  // white
+    vec4(0.0, 1.0, 1.0, 1.0)   // cyan
+];
+
+let xAxis = 0;
+let yAxis = 1;
+let zAxis = 2;
+let axis = xAxis;
+let theta = vec3(45.0, 45.0, 45.0);
+
+let thetaLoc;
+
+function configureTexture( image, program ) {
+    texture = gl.createTexture();
+    gl.activeTexture( gl.TEXTURE0 );  //0 active by default
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    
+    //Flip the Y values to match the WebGL coordinates
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+    
+    //Specify the image as a texture array:
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
+         
+    //Set filters and parameters
+    gl.generateMipmap(gl.TEXTURE_2D);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST_MIPMAP_LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    
+    //Link texture to a sampler in fragment shader
+    gl.uniform1i(gl.getUniformLocation(program, "u_textureMap"), 0);
+}
+
+
+function quad(a, b, c, d) {
+     positionsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[0]);
+
+     positionsArray.push(vertices[b]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[1]);
+
+     positionsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[2]);
+
+     positionsArray.push(vertices[a]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[0]);
+
+     positionsArray.push(vertices[c]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[2]);
+
+     positionsArray.push(vertices[d]);
+     colorsArray.push(vertexColors[a]);
+     texCoordsArray.push(texCoord[3]);
+}
+
+
+function colorCube(){
+    quad(1, 0, 3, 2);
+    quad(2, 3, 7, 6);
+    quad(3, 0, 4, 7);
+    quad(6, 5, 1, 2);
+    quad(4, 5, 6, 7);
+    quad(5, 4, 0, 1);
+}
+
+
+function init() {
+	//Get graphics context
+    let canvas = document.getElementById( "gl-canvas" );
+	let  options = {  // no need for alpha channel, but note depth buffer enabling
+		alpha: false,
+		depth: true  //NOTE THIS
+	};
+
+	gl = canvas.getContext("webgl2", options);
+    if ( !gl ) { alert( "WebGL 2.0 isn't available" ); }
+
+	//Load shaders
+	let program = initShaders( gl, "vertex-shader", "fragment-shader" );
+	gl.useProgram( program );
+
+    colorCube();
+
+    let cBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
+
+    let colorLoc = gl.getAttribLocation(program, "a_color");
+    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(colorLoc);
+
+    let vBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
+
+    let positionLoc = gl.getAttribLocation(program, "a_coords");
+    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(positionLoc);
+
+    let tBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
+
+    let texCoordLoc = gl.getAttribLocation(program, "a_texCoord");
+    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(texCoordLoc);
+
+    //Initialize texture
+    let image = new Image();
+    image.src = document.getElementById("texImage").src; 
+    image.onload = function() {
+        configureTexture( image, program );
+    }
+    
+    thetaLoc = gl.getUniformLocation(program, "u_theta");
+
+    document.getElementById("ButtonX").onclick = function(){axis = xAxis;};
+    document.getElementById("ButtonY").onclick = function(){axis = yAxis;};
+    document.getElementById("ButtonZ").onclick = function(){axis = zAxis;};
+    document.getElementById("ButtonT").onclick = function(){flag = !flag;};
+
+
+    //set up screen
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height); 
+    gl.clearColor(1, 1, 1, 1); 
+    
+    //Enable depth testing    
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.POLYGON_OFFSET_FILL);
+    gl.polygonOffset(1.0, 2.0);     
+    
+    draw();
+
+}
+
+function draw() {
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    if(flag) 
+            theta[axis] += 1.0;
+    gl.uniform3fv(thetaLoc, theta);
+    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+    requestAnimationFrame(draw);
+}
