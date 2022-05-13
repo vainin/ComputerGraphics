3938852fbd9f707f0b2c443405f602773bd81d3d
Benjamin <vainin@users.noreply.github.com>
2022-05-12 19:05:10 -0500

    Combined earlier versions

--- Arena.js
+++ Arena.js
@@ -1,19 +1,38 @@
 "use strict";
 
+let canvas;
 let gl;
+const TIME_LIMIT = 10;
 
-let timelimit = 60;
+let near = -100;
+let far = 100;
+let left = -100.0;
+let right = 100.0;
+let ytop = 100.0;
+let bottom = -100.0;
+
+//
+let timelimit = 10;
 let timegone = 0;
 let timer;
-let shootable = false;
+let shootable = true;
+let runningGame = false;
+let score = 0;
+let scoreText = document.querySelector("#userscore");
 
 //above is needed code
+//Below was combined
+let radius = 40.0;
+let theta = 0.0;
+let phi = 0.0;
+let rotation_by_5_deg = 5.0 * Math.PI / 180.0;
+let done = false;
+let at = vec3(0.0, 0.0, 0.0);
+let up = vec3(0.0, 1.0, 0.0);
 
-let numPositions  = 36;
-
-let texSize = 64;
-
-let flag = true;
+let uniformModelView, uniformProjection;
+let modelViewMatrix, projectionMatrix;
+let uniformColor;
 
 let positionsArray = [];
 let colorsArray = [];
@@ -21,44 +40,58 @@ let texCoordsArray = [];
 
 let texture;
 
+let coords = myHoop.vertices[0].values;
+let indices = myHoop.connectivity[0].indices;
+
+let normals;
+
+let program;
+let vaos = [];
+let shapes;
+
+let tex;
+
+let machine = {
+    positions: coords,
+    normals: coords,
+    texcoords: coords,
+    indices: indices,
+};
+
+let coords2 = shoeMesh.vertices[0].values;
+let indices2 = shoeMesh.connectivity[0].indices;
+
+let shoe = {
+    positions: coords2,
+    normals: coords2,
+    texcoords: coords2,
+    indices: indices2,
+}
 
 
-let texCoord = [
-    vec2(0, 0),
-    vec2(0, 1),
-    vec2(1, 1),
-    vec2(1, 0)
-];
+//light 
+let lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0); // white light
+let lightAmbient = vec4(0.2, 0.2, 0.2, 1.0);
+let lightSpecular = vec4(0.9, 0.9, 0.9, 1.0);
 
-let vertices = [
-    vec4(-0.5, -0.5,  0.5, 1.0),
-    vec4(-0.5,  0.5, 0.5, 1.0),
-    vec4(0.5,  0.5, 0.5, 1.0),
-    vec4(0.5, -0.5, 0.5, 1.0),
-    vec4(-0.5, -0.5, -0.5, 1.0),
-    vec4(-0.5,  0.5, -0.5, 1.0),
-    vec4(0.5,  0.5, -0.5, 1.0),
-    vec4(0.5, -0.5, -0.5, 1.0)
-];
+let lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
 
-let vertexColors = [
-    vec4(0.0, 0.0, 0.0, 1.0),  // black
-    vec4(1.0, 0.0, 0.0, 1.0),  // red
-    vec4(1.0, 1.0, 0.0, 1.0),  // yellow
-    vec4(0.0, 1.0, 0.0, 1.0),  // green
-    vec4(0.0, 0.0, 1.0, 1.0),  // blue
-    vec4(1.0, 0.0, 1.0, 1.0),  // magenta
-    vec4(0.0, 1.0, 1.0, 1.0),  // white
-    vec4(0.0, 1.0, 1.0, 1.0)   // cyan
-];
+let materialDiffuse = [vec4(0, 0.2, 0, 1.0),
+vec4(0.2, 1, 1.0, 1.0),
+vec4(0.2, 0.2, 1, 1.0),
+vec4(.75, .24, 1.0, 1.0)];
 
-let xAxis = 0;
-let yAxis = 1;
-let zAxis = 2;
-let axis = xAxis;
-let theta = vec3(45.0, 45.0, 45.0);
+let materialAmbient = [vec4(1, .5, 1.0, 1.0),
+vec4(0.2, 1, 1.0, 1.0),
+vec4(0.2, 0.2, 1, 1.0),
+vec4(.75, .24, 1, 1.0)];
+let materialSpecular = [vec4(1, 1, 1.0, 1.0),
+vec4(0.2, 1, 1.0, 1.0),
+vec4(0.2, 0.2, 1, 1.0),
+vec4(.75, .24, 1, 1.0)];
+let materialShininess = 100.0;
 
-let thetaLoc;
+
 
 //Added May 9
 let materialDiffuseBall =  vec4( 1.0, 0.65, 0.0, 1.0); 
@@ -89,44 +122,6 @@ function configureTexture( image, program ) {
     gl.uniform1i(gl.getUniformLocation(program, "u_textureMap"), 0);
 }
 
-
-function quad(a, b, c, d) {
-     positionsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[0]);
-
-     positionsArray.push(vertices[b]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[1]);
-
-     positionsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]);
-
-     positionsArray.push(vertices[a]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[0]);
-
-     positionsArray.push(vertices[c]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[2]);
-
-     positionsArray.push(vertices[d]);
-     colorsArray.push(vertexColors[a]);
-     texCoordsArray.push(texCoord[3]);
-}
-
-
-function colorCube(){
-    quad(1, 0, 3, 2);
-    quad(2, 3, 7, 6);
-    quad(3, 0, 4, 7);
-    quad(6, 5, 1, 2);
-    quad(4, 5, 6, 7);
-    quad(5, 4, 0, 1);
-}
-
-
 function init() {
 	//Get graphics context
     let canvas = document.getElementById( "gl-canvas" );
@@ -138,44 +133,47 @@ function init() {
 	gl = canvas.getContext("webgl2", options);
     if ( !gl ) { alert( "WebGL 2.0 isn't available" ); }
 
-	//Load shaders
-	let program = initShaders( gl, "vertex-shader", "fragment-shader" );
-	gl.useProgram( program );
+    //Load shaders
+    program = initShaders(gl, "vertex-shader", "fragment-shader");
+    gl.useProgram(program);
 
-    colorCube();
+    shapes = [
+        [machine, mat4()],
+        [shoe,  mat4()],
+        [createSphereVertices(5, 30, 30),  mat4()]
+    ];
 
-    let cBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(colorsArray), gl.STATIC_DRAW );
-
-    let colorLoc = gl.getAttribLocation(program, "a_color");
-    gl.vertexAttribPointer(colorLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(colorLoc);
-
-    let vBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(positionsArray), gl.STATIC_DRAW);
-
-    let positionLoc = gl.getAttribLocation(program, "a_coords");
-    gl.vertexAttribPointer(positionLoc, 4, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(positionLoc);
-
-    let tBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, tBuffer);
-    gl.bufferData(gl.ARRAY_BUFFER, flatten(texCoordsArray), gl.STATIC_DRAW);
-
-    let texCoordLoc = gl.getAttribLocation(program, "a_texCoord");
-    gl.vertexAttribPointer(texCoordLoc, 2, gl.FLOAT, false, 0, 0);
-    gl.enableVertexAttribArray(texCoordLoc);
-
-    //Initialize texture
-    let image = new Image();
-    image.src = document.getElementById("texImage").src; 
-    image.onload = function() {
-        configureTexture( image, program );
-    }
+   
     
-    thetaLoc = gl.getUniformLocation(program, "u_theta");
+    setUpShapes();
+
+
+       //Initialize texture
+       let image = new Image();
+       image.src = document.getElementById("texImage").src; 
+       image.onload = function() {
+           configureTexture( image, program );
+       }
+
+
+
+    for (let i = 0; i < shapes.length; i++) {
+        vaos.push(setUpVertexObject(shapes[i][0]));
+    }
+
+    let pic = new Image();
+    pic.src = document.getElementById("image").src;
+    pic.onload = function () {
+        configureTexture(pic, program);
+    }
+
+    //set up uniform variables
+    uniformModelView = gl.getUniformLocation(program, "u_modelViewMatrix");
+    uniformProjection = gl.getUniformLocation(program, "u_projectionMatrix");
+
+    document.onkeydown = function (ev) { keydown(ev); };
+    
+
 
     
 
@@ -193,6 +191,22 @@ function init() {
 
 }
 
+function setUpShapes(){
+    //shapes[1][1] = mult(shapes[1][1], rotateZ(90));
+    //shapes[1][1] = mult(shapes[1][1], rotateY(90));
+    //shapes[1][1] = mult(shapes[1][1], rotateZ(90));
+    shapes[1][1] = mult(shapes[1][1],translate(10,-10,0));
+    //shapes[0][1] = mult(shapes[0][1],translate(1,0,-10));
+    //shapes[0][1] = mult(shapes[0][1],scalem(5,5,5));
+    shapes[0][1] = mult(shapes[0][1],rotateY(90));
+    shapes[2][1] = mult(shapes[2][1],translate(0,-90,0));
+    //shapes[2][1] = mult(shapes[2][1],scalem(5,5,5));
+
+}
+
+function lR() {
+    radius *= 0.9;
+}
 
 function drawVertexObject(vao, iLength, mA, mD, mS, s){
     let ambientProduct = mult(lightAmbient, mA);
@@ -245,18 +259,80 @@ function setUpVertexObject(shape){
 
 function draw() {
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    if(flag) 
-            theta[axis] += 1.0;
-    gl.uniform3fv(thetaLoc, theta);
-    gl.drawArrays(gl.TRIANGLES, 0, numPositions);
+
+    let eye = vec3(radius * Math.sin(theta) * Math.cos(phi),
+        radius * Math.sin(theta) * Math.sin(phi),
+        radius * Math.cos(theta));
+
+    modelViewMatrix = lookAt(eye, at, up);
+    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+
+    //gl.uniformMatrix4fv( uniformModelView, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv(uniformProjection, false, flatten(projectionMatrix));
+
+
+    //gl.drawElements( gl.LINES, indices.length, gl.UNSIGNED_SHORT, 0 ); 
+
+    //gl.uniform4fv(gl.getUniformLocation(program, "lightPosition"), flatten(lightPosition));
+    for (let i = 0; i < shapes.length; i++) {
+        modelViewMatrix = (mult(modelViewMatrix, shapes[i][1]));
+        gl.uniformMatrix4fv(uniformModelView, false, flatten(modelViewMatrix));
+        drawVertexObject(vaos[i], shapes[i][0].indices.length, materialAmbient[i], materialDiffuse[i], materialSpecular[i], materialShininess / (i + 1));
+    }
+    
     requestAnimationFrame(draw);
+}
+
+// This function tries to guess what the appropriate viewing
+// parameters should be based on the overall values of the
+// coordinates
+function setViewParams(vertices) {
+    let xmin = Infinity;
+    let xmax = -Infinity;
+    let ymin = Infinity;
+    let ymax = -Infinity;
+    let zmin = Infinity;
+    let zmax = -Infinity;
+    for (let i = 0; i < vertices.length; i = i + 3) {
+        if (vertices[i] < xmin)
+            xmin = vertices[i];
+        else if (vertices[i] > xmax)
+            xmax = vertices[i];
+        if (vertices[i + 1] < ymin)
+            ymin = vertices[i + 1];
+        else if (vertices[i + 1] > ymax)
+            ymax = vertices[i + 1];
+        if (vertices[i + 2] < zmin)
+            zmin = vertices[i + 2];
+        else if (vertices[i + 2] > zmax)
+            zmax = vertices[i + 2];
+    }
+
+    /* translate the center of the object to the origin */
+    let centerX = (xmin + xmax) / 2;
+    let centerY = (ymin + ymax) / 2;
+    let centerZ = (zmin + zmax) / 2;
+    let max = Math.max(centerX - xmin, xmax - centerX);
+    max = Math.max(max, Math.max(centerY - ymin, ymax - centerY));
+    max = Math.max(max, Math.max(centerZ - zmin, zmax - centerZ));
+    let margin = max * 0.2;
+    left = -(max + margin);
+    right = max + margin;
+    bottom = -(max + margin);
+    ytop = max + margin;
+    far = -(max + margin);
+    near = max + margin;
+    radius = max + margin;
 }
 
 //below is needed code
 function startgame(){
 	resetgame();
 	clearInterval(timer);
+	runningGame = true;
 	timer = setInterval(settimer,1000); 
+	var x = document.getElementById("startButton");
+    	x.style.display = "none";
 
 }
 function settimer(){
@@ -270,26 +346,97 @@ function settimer(){
 	}
 }
 function resetgame(){
-	timelimit = 60;
+	timelimit = 10;
 	timegone = 0;
+	score = 0;
+    	scoreText.value = score;
 	shootable = true;
 	document.getElementById("userscore").value = "0";
 	document.getElementById("timertext").Text = timelimit;
+	
+	    var x = document.getElementById("restartButton");
+    	x.style.display = "none";
+    	x = document.getElementById("startButton");
+    	x.style.display = "block";
+    	runningGame = false;
+
 	//need to disable startbutton until game ends
 	
 }
 function endgame(){
 	clearInterval(timer);
+	runningGame = false;
 	shootable = false;
+	var x = document.getElementById("startButton");
+    	x.style.display = "none";
+    	x = document.getElementById("restartButton");
+    	x.style.display = "block";
 	//enable startbutton
 	//compare scores to update highscore
+	if(document.getElementById("highscore").value <  document.getElementById("userscore").value){
+        document.getElementById("highscore").value = score;
+    }
 	
 }
 
 function shoot(){
-	if(shootable){
-		//needs create basketball that moves towards hoop
-	}
+	if (shootable && runningGame) {
+        shootable = false;
+        //needs create basketball that moves towards hoop
+        shapes[2][1] = mult(shapes[2][1],translate(0,50,0));
+        draw()
+
+        //see if blocked 
+        sleep(100);
+        let t = (Math.floor(Math.random() * 100) + 1);
+        if (t % 2 === 0) {
+            score = score - 75;
+            scoreText.value = score;
+            shapes[2][1] = mult(shapes[2][1],translate(0,25,0));
+            shapes[1][1] = mult(shapes[1][1],translate(0,0,25));
+            draw();
+            document.getElementById('image2').style.display = 'block';
+            sleep(100);
+            shapes[1][1] = mult(shapes[1][1],translate(0,0,-25));
+            draw();
+            sleep(100);
+            
+        } else {
+            document.getElementById('image2').style.display = 'none';
+            lightShow();
+            shapes[2][1] = mult(shapes[2][1],translate(0,25,0));
+            draw();
+            score = score + 100;
+            scoreText.value = score;
+            sleep(100);
+        }
+    }
+    shapes[2][1] = mult(shapes[2][1],translate(0,-75,0));
+    shootable = true;
+}
+
+async function lightShow() {
+    lightDiffuse = vec4(0, 1.0, 1.0, 1.0);
+    lightPosition = vec4(0.5, 1.0, 1.0, 0.0);
+    await sleep(70);
+    lightDiffuse = vec4(0, 1.0, 1.0, 1.0);
+    lightPosition = vec4(0.75, 1.0, 1.0, 0.0);
+    await sleep(70);
+    lightDiffuse = vec4(1.0, 0, 1.0, 1.0);
+    lightPosition = vec4(1.0, 1.0, 0.0, 0.0);
+    await sleep(70);
+    lightDiffuse = vec4(0, 1.0, 1.0, 1.0);
+    lightPosition = vec4(1.0, 1.0, 0.75, 0.0);
+    await sleep(70);
+    lightDiffuse = vec4(1.0, 1.0, 0, 1.0);
+    lightPosition = vec4(1.0, 0.0, 1.0, 0.0);
+    await sleep(70);
+    lightDiffuse = vec4(1.0, 1.0, 1.0, 1.0);
+    lightPosition = vec4(1.0, 1.0, 1.0, 0.0);
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 function keydown(ev) {
